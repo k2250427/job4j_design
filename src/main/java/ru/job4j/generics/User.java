0dc6929ee13c128ade742f31711e962bc9f538ae
Stasyuk.K <kstasyuk@ya.ru>
2021-06-24 15:46:00 +0300

    5.2.2. Реализовать Store<T extends Base> [#157]

--- src/main/java/ru/job4j/generics/User.java
+++ src/main/java/ru/job4j/generics/User.java
@@ -1,8 +1,8 @@
 package ru.job4j.generics;
 
-public class User extends Base{
+public class User extends Base {
 
-    public User(){
+    public User() {
         super(Long.toString(Math.round(Math.random() * 1000000)));
     }
 }
