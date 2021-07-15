3388912dc31ba9d831ccb8031fd71e05a5e14ac0
Stasyuk.K <kstasyuk@ya.ru>
2021-07-15 11:28:16 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008 #177611]

---
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -0,0 +1,135 @@
+package ru.job4j.map;
+
+import java.util.*;
+
+public class SimpleMap<K, V> implements Map<K, V> {
+
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private int capacity = 8;
+
+    private int count = 0;
+
+    private int modCount = 0;
+
+    private MapEntry<K, V>[] table = new MapEntry[capacity];
+
+    @Override
+    public boolean put(K key, V value) {
+        if (count == 0 || get(key) == null) {
+            int n = indexFor(hash(Objects.hashCode(key)));
+            if (table[n] == null) {
+                table[n] = new MapEntry<>(key, value);
+                count++;
+                modCount++;
+                if ((float) count / capacity > LOAD_FACTOR) {
+                    expand();
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int hash(int hashCode) {
+        return Math.abs(hashCode ^ hashCode >>> 16);
+    }
+
+    private int indexFor(int hash) {
+        return hash % capacity;
+    }
+
+    private void expand() {
+        capacity *= 2;
+        MapEntry<K, V>[] newTable = new MapEntry[capacity];
+        for (MapEntry<K, V> el: table) {
+            if (el == null) {
+                continue;
+            }
+            int n = indexFor(hash(Objects.hashCode(el.key)));
+            newTable[n] = new MapEntry<>(el.key, el.value);
+        }
+        table = newTable;
+    }
+
+    @Override
+    public V get(K key) {
+        for (MapEntry<K, V> el: table) {
+            if (el == null) {
+                continue;
+            }
+            if (Objects.equals(el.key, key)) {
+                return el.value;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean remove(K key) {
+        if (get(key) == null) {
+            return false;
+        }
+        for (int i = 0; i < capacity; i++) {
+            if (table[i] == null) {
+                continue;
+            }
+            if (Objects.equals(table[i].key, key)) {
+                System.arraycopy(table, i + 1, table, i, capacity - i - 1);
+                table[capacity - 1] = null;
+                count--;
+                modCount++;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return new SimpleMapIterator(this);
+    }
+
+    class SimpleMapIterator implements Iterator<K> {
+
+        private SimpleMap<K, V> arr;
+        private int current;
+        private int modCount;
+
+        SimpleMapIterator(SimpleMap<K, V> arr) {
+            this.arr = arr;
+            this.current = 0;
+            this.modCount = arr.modCount;
+        }
+
+        public K next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return arr.table[current++].key;
+        }
+
+        public boolean hasNext() {
+            if (modCount != arr.modCount) {
+                throw new ConcurrentModificationException();
+            }
+            while (current < arr.capacity && arr.table[current] == null) {
+                current++;
+            }
+            return current < arr.capacity && arr.table[current] != null;
+        }
+
+    }
+
+    private static class MapEntry<K, V> {
+
+        private K key;
+        private V value;
+
+        public MapEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/map/SimpleMapTest.java
@@ -0,0 +1,102 @@
+package ru.job4j.map;
+
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+public class SimpleMapTest {
+
+    @Test
+    public void whenPutNewKey() {
+        SimpleMap<String, Integer> map = new SimpleMap<>();
+        assertTrue(map.put("Tver", 120));
+        assertTrue(map.put("Samara", 400));
+        assertTrue(map.put("Petrozavodsk", 80));
+    }
+
+    @Test
+    public void whenPutNewKeyWithExpand() {
+        SimpleMap<String, Integer> map = new SimpleMap<>();
+        assertTrue(map.put("Tver", 120));
+        assertTrue(map.put("Samara", 400));
+        assertTrue(map.put("Petrozavodsk", 80));
+        assertTrue(map.put("Lipetsk", 100));
+        assertTrue(map.put("Tula", 200));
+        assertTrue(map.put("Kaluga", 180));
+        assertTrue(map.put("Ivanovo", 180));
+        assertTrue(map.put("Emtsa", 130));
+    }
+
+    @Test
+    public void whenPutExistingKey() {
+        SimpleMap<String, Integer> map = new SimpleMap<>();
+        assertTrue(map.put("Tver", 120));
+        assertTrue(map.put("Samara", 400));
+        assertTrue(map.put("Petrozavodsk", 80));
+        assertFalse(map.put("Tver", 120));
+    }
+
+    @Test
+    public void whenGetExistingKey() {
+        SimpleMap<String, Integer> map = new SimpleMap<>();
+        assertTrue(map.put("Tver", 120));
+        assertTrue(map.put("Samara", 400));
+        assertTrue(map.put("Petrozavodsk", 80));
+        assertEquals(map.get("Samara"), (Integer) 400);
+    }
+
+    @Test
+    public void whenGetMissingKey() {
+        SimpleMap<String, Integer> map = new SimpleMap<>();
+        assertTrue(map.put("Tver", 120));
+        assertTrue(map.put("Samara", 400));
+        assertTrue(map.put("Petrozavodsk", 80));
+        assertNull(map.get("Moscow"));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenRemoveExistingKey() {
+        SimpleMap<String, Integer> map = new SimpleMap<>();
+        assertTrue(map.put("Tver", 120));
+        assertTrue(map.put("Samara", 400));
+        assertTrue(map.put("Petrozavodsk", 80));
+        assertTrue(map.remove("Samara"));
+        Iterator<String> it = map.iterator();
+        assertEquals(map.get(it.next()), (Integer) 120);
+        assertEquals(map.get(it.next()), (Integer) 80);
+        it.next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenRemoveMissingKey() {
+        SimpleMap<String, Integer> map = new SimpleMap<>();
+        assertTrue(map.put("Tver", 120));
+        assertTrue(map.put("Samara", 400));
+        assertTrue(map.put("Petrozavodsk", 80));
+        assertFalse(map.remove("Moscow"));
+        Iterator<String> it = map.iterator();
+        assertEquals(map.get(it.next()), (Integer) 120);
+        assertEquals(map.get(it.next()), (Integer) 400);
+        assertEquals(map.get(it.next()), (Integer) 80);
+        it.next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenIterateOverMap() {
+        SimpleMap<String, Integer> map = new SimpleMap<>();
+        assertTrue(map.put("Tver", 120));
+        assertTrue(map.put("Samara", 400));
+        assertTrue(map.put("Petrozavodsk", 80));
+        Iterator<String> it = map.iterator();
+        assertEquals(map.get(it.next()), (Integer) 120);
+        assertEquals(map.get(it.next()), (Integer) 400);
+        assertEquals(map.get(it.next()), (Integer) 80);
+        it.next();
+    }
+}
