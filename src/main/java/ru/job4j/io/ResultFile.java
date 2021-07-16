b6239ebe5b390347d410259473bfd011e2b6e463
Stasyuk.K <kstasyuk@ya.ru>
2021-07-16 14:21:19 +0300

    0.1. FileOutputStream. [#252488 #178022]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -7,7 +7,7 @@ public class ResultFile {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
             for (int i = 1; i < 10; i++) {
                 for (int k = 1; k < 10; k++) {
-                out.write(("" + i + " * " + k + " = " + i * k + "\n").getBytes());
+                out.write(("" + i + " * " + k + " = " + i * k + System.lineSeparator()).getBytes());
                 }
                 out.write("\n".getBytes());
             }
