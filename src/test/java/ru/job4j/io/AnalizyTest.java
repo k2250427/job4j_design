470ef4d9c5bf306a15f217342cdc7d3ce63b9382
CS <kstasyuk@ya.ru>
2021-07-19 22:57:50 +0300

    3.0. Тестирование IO [#173905]

---
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.io;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class AnalizyTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void unavailable() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("200 10:56:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
+        }
+        Analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                rsl.append(line);
+                rsl.append("\n");
+            }
+        }
+        assertThat(rsl.toString(), is("10:57:01;10:59:01\n11:01:02;11:02:02\n"));
+    }
+}
