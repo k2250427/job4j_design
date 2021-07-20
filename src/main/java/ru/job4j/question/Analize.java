d42e0def77792874f6df525af9eaa8c40e3873ee
Stasyuk.K <kstasyuk@ya.ru>
2021-07-20 14:58:26 +0300

    2. Статистика по коллекции. [#45889 #178014]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -8,18 +8,14 @@ public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
         HashMap<Integer, String> curr = new HashMap<>();
-        int upd = 0, del = 0;
-        Set<Integer> prevkeys = new HashSet<>();
-        Set<Integer> curkeys = new HashSet<>();
+        int add, upd = 0, del = 0;
 
         for (User user: current) {
             curr.put(user.getId(), user.getName());
-            curkeys.add(user.getId());
         }
 
         for (User user : previous) {
             String name = curr.get(user.getId());
-            prevkeys.add(user.getId());
             if (name != null) {
                 if (!name.equals(user.getName())) {
                     upd++;
@@ -28,7 +24,7 @@ public class Analize {
                 del++;
             }
         }
-        curkeys.removeAll(prevkeys);
-        return new Info(curkeys.size(), upd, del);
+        add = current.size() + del - previous.size();
+        return new Info(add, upd, del);
     }
 }
