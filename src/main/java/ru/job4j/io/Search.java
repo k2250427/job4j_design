05077c1355365ca2f7f5d189a9b2f76ac7527adb
Stasyuk.K <kstasyuk@ya.ru>
2021-07-20 17:11:38 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -11,8 +11,11 @@ import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("xml")).forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Invalid arguments. Usage java -jar search.jar ROOT_FOLDER FILE_EXTENSION");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
