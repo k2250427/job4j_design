d015b787ac2343025d15f42755e72762e7eb042a
Stasyuk.K <kstasyuk@ya.ru>
2021-07-28 10:34:42 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,6 +3,7 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -12,13 +13,18 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty();
                                                                 str = in.readLine()) {
                         System.out.println(str);
-                        if (str.contains("msg=Bye")) {
+                        if (str.contains("msg=Exit")) {
                             socket.close();
                             server.close();
+                            break;
+                        } else if (str.contains("msg=Hello")) {
+                            out.write("Hello\r\n".getBytes());
+                        } else if (str.contains("msg=")) {
+                            out.write("What\r\n".getBytes());
                         }
                     }
                     out.flush();
