98d95594157c2f944b8aaffad9b3020e1bac2371
Stasyuk.K <kstasyuk@ya.ru>
2021-07-29 08:44:51 +0300

    1. Бот [#7921 #182672]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -17,7 +17,7 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty();
                                                                 str = in.readLine()) {
                         System.out.println(str);
